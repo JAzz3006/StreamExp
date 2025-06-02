587004c1a7e1e6451bd83981f876b9d311055276
Grigoriy Chernykh <che90@mail.ru>
2025-06-02 19:29:59 +0500

    home commit 2/06 flatmap training

--- src/main/java/org/example/Tst2.java
+++ src/main/java/org/example/Tst2.java
@@ -6,6 +6,51 @@ import java.util.stream.Collectors;
 public class Tst2 {
     public static final List<String> VOWELS_LIST = List.of("a", "e", "i", "o", "u", "y");
     public static void main(String[] args) {
+        //начало задачи на флатмап №4
+        List<String> sentences = List.of(
+                "Java is cool",
+                "Streams are powerful in Java",
+                "Java and flatMap are amazing"
+        );
+        TreeSet<String> wordSet = sentences.stream()
+                .map(String::toLowerCase)
+                .flatMap(a -> Arrays.stream(a.split("\\s+")))
+                //.distinct()
+                .collect(TreeSet::new,
+                        TreeSet::add,
+                        TreeSet::addAll
+                );
+        System.out.println(wordSet);
+
+        //конец задачи на флатмап №4
+        //начало задачи на флатмап №3
+        List<String> linez = List.of(
+                "Order #1234: 3 items",
+                "Invoice 567 and 89",
+                "No digits here!",
+                "Final code: 42"
+        );
+        List<String> digits = linez.stream()
+                .map(s -> s.split(""))
+                .flatMap(Arrays::stream)
+                .filter(s -> Character.isDigit(s.charAt(0)))
+                .toList();
+        //System.out.println(digits);
+        //конец задачи на флатмап №3
+
+        //начало задачи на флатмап №2
+        List<List<String>> wordGroups = List.of(
+                List.of("elephant", "tiger"),
+                List.of("cat", "dog", "mouse"),
+                List.of("hippo"),
+                List.of("giraffe", "lion", "goat")
+        );
+        List<String> splitList = wordGroups.stream()
+                .flatMap(Collection::stream)
+                .map(String::toUpperCase)
+                .toList();
+        //splitList.forEach(System.out::println);
+        //конец задачи на флатмап №2
         //начало задачи про разворот перечня из слов
         List<String> phrases = List.of(
                 "elephant and tiger",
@@ -13,7 +58,10 @@ public class Tst2 {
                 "hippo",
                 "giraffe lion goat"
         );
-
+        List<String> splitted = phrases.stream()
+                .flatMap(s -> Arrays.stream(s.split("\\s+")))
+                .toList();
+        //splitted.forEach(System.out::println);
         //конец задачи про разворот перечня из слов
 
         //начало задачи про группировку слов
@@ -26,7 +74,7 @@ public class Tst2 {
                         TreeMap::new,
                         Collectors.toList()
                 ));
-        wordsMap.forEach((key, value) -> System.out.println(key + " - " + value));
+        //wordsMap.forEach((key, value) -> System.out.println(key + " - " + value));
         //конец задачи про группировку слов
 
         //начало задачи по максимум гласных
